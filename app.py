64f11ff54b19b0ca6c32d0feb6b01843089b69f6
Zuhayr <ayaanza29@gmail.com>
2022-01-08 20:30:13 -0500

    flask

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, request, redirect, url_for, render_template
 import datetime
+import os
 from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
-app.config.from_pyfile("config.py")
+app.config["Upload_Folder"] = UPLOAD_FOLDER
 
 @app.route("/")
 def index():
@@ -43,7 +48,7 @@ def settings():
 #@app.route('/upload.html')
 #def upload():
 #    return render_template('upload.html')
-
+""""
 @app.route('/upload')
 def upload_file():
    return render_template('upload.html')
@@ -54,6 +59,39 @@ def upload_file():
       f = request.files['file']
       f.save(secure_filename(f.filename))
       return 'file uploaded successfully'
+"""
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
 
 #pytho -m venv venv
 #venv\Scripts\activate
