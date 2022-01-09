07dc0a3c3f30084f7d5a210708cde37b35a20980
Zuhayr <ayaanza29@gmail.com>
2022-01-08 20:32:15 -0500

    uploading

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload.html', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -98,4 +98,4 @@ def upload_file():
 #localhost:5000
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
