5defa6409c1fd2c94a74c6fe35596b9239066873
Zuhayr <ayaanza29@gmail.com>
2022-01-08 20:14:53 -0500

    importing

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import datetime
+from werkzeug import secure_filename
 app = Flask(__name__)
+app.config["C:\\Users\\rkhan\\Desktop\\Z Research Programming"]
 
 @app.route("/")
 def index():
@@ -38,9 +40,20 @@ def pca():
 def settings():
     return render_template('settings.html')
 
-@app.route('/upload.html')
-def upload():
-    return render_template('upload.html')
+#@app.route('/upload.html')
+#def upload():
+#    return render_template('upload.html')
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
 
 #pytho -m venv venv
 #venv\Scripts\activate
